ba07512235a157357855c697498611d15987e9fe
Martin Bauer <bauer_martin@icloud.com>
2019-03-31 17:51:12 +0200

    Add OptiMathSat tests for solution count

---
+++ tests/opti_math_sat/SolutionCountTestCase.cpp
@@ -0,0 +1,23 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <iostream>
+
+#include "opti_math_sat/OptiMathSatSolverFactory.h"
+#include "spl_conqueror/VariabilityModel.h"
+#include "spl_conqueror/VariantGenerator.h"
+
+int test_case(const std::string &feature_model, int expected_solution_count) {
+  boost::property_tree::ptree tree;
+  boost::property_tree::read_xml("feature-models/" + feature_model, tree);
+  spl_conqueror::VariabilityModel *model = spl_conqueror::VariabilityModel::make_from_xml(tree);
+  opti_math_sat::OptiMathSatSolverFactory solver_factory(*model);
+  spl_conqueror::VariantGenerator *vg = solver_factory.make_variant_generator();
+  const std::vector<std::vector<spl_conqueror::BinaryOption *>> &solutions = vg->generate_up_to_n_configs(-1);
+  int actual_solution_count = solutions.size();
+  if (actual_solution_count == expected_solution_count) {
+    return EXIT_SUCCESS;
+  } else {
+    std::cerr << "Expected: " << expected_solution_count << ", but got: " << actual_solution_count << std::endl;
+    return EXIT_FAILURE;
+  }
+}
