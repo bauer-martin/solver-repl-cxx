e9584aa90a7679015d275230be6b2d586938bcca
Martin Bauer <bauer_martin@icloud.com>
2019-04-06 09:58:47 +0200

    Rename variable

--- cmake/Modules/FindORTools.cmake
+++ cmake/Modules/FindORTools.cmake
@@ -1,30 +1,30 @@
 # Try to find Google OR-Tools (https://developers.google.com/optimization/)
 #
-# Requires ORTOOLS_ROOT to be set.
+# Requires OR_TOOLS_ROOT to be set.
 #
 # Once done this will define
 #  ORTools_FOUND        - True if headers and libraries were found
 #  ORTools_INCLUDE_DIRS - OR-Tools include directories
 #  ORTools_LIBRARIES    - OR-Tools libraries to be linked
 
-if (NOT DEFINED ORTOOLS_ROOT OR ORTOOLS_ROOT STREQUAL "")
-  message(FATAL_ERROR "ORTOOLS_ROOT was not given!")
+if (NOT DEFINED OR_TOOLS_ROOT OR OR_TOOLS_ROOT STREQUAL "")
+  message(FATAL_ERROR "OR_TOOLS_ROOT was not given!")
 endif ()
 
-find_path(ORTools_INCLUDE_DIR ortools PATHS "${ORTOOLS_ROOT}/include")
+find_path(ORTools_INCLUDE_DIR ortools PATHS "${OR_TOOLS_ROOT}/include")
 
-find_library(Cbc_LIBRARY Cbc PATHS "${ORTOOLS_ROOT}/lib")
-find_library(CbcSolver_LIBRARY CbcSolver PATHS "${ORTOOLS_ROOT}/lib")
-find_library(Cgl_LIBRARY Cgl PATHS "${ORTOOLS_ROOT}/lib")
-find_library(Clp_LIBRARY Clp PATHS "${ORTOOLS_ROOT}/lib")
-find_library(ClpSolver_LIBRARY ClpSolver PATHS "${ORTOOLS_ROOT}/lib")
-find_library(CoinUtils_LIBRARY CoinUtils PATHS "${ORTOOLS_ROOT}/lib")
-find_library(gflags_LIBRARY gflags PATHS "${ORTOOLS_ROOT}/lib")
-find_library(glog_LIBRARY glog PATHS "${ORTOOLS_ROOT}/lib")
-find_library(OsiCbc_LIBRARY OsiCbc PATHS "${ORTOOLS_ROOT}/lib")
-find_library(OsiClp_LIBRARY OsiClp PATHS "${ORTOOLS_ROOT}/lib")
-find_library(protobuf_LIBRARY protobuf PATHS "${ORTOOLS_ROOT}/lib")
-find_library(ortools_LIBRARY ortools PATHS "${ORTOOLS_ROOT}/lib")
+find_library(Cbc_LIBRARY Cbc PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(CbcSolver_LIBRARY CbcSolver PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(Cgl_LIBRARY Cgl PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(Clp_LIBRARY Clp PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(ClpSolver_LIBRARY ClpSolver PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(CoinUtils_LIBRARY CoinUtils PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(gflags_LIBRARY gflags PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(glog_LIBRARY glog PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(OsiCbc_LIBRARY OsiCbc PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(OsiClp_LIBRARY OsiClp PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(protobuf_LIBRARY protobuf PATHS "${OR_TOOLS_ROOT}/lib")
+find_library(ortools_LIBRARY ortools PATHS "${OR_TOOLS_ROOT}/lib")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ORTools DEFAULT_MSG
