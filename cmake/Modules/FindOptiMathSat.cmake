901e08aa424ecc231b698564aced5f16fbbb7b3c
Martin Bauer <bauer_martin@icloud.com>
2019-04-06 09:53:04 +0200

    Require root directory for solvers

--- cmake/Modules/FindOptiMathSat.cmake
+++ cmake/Modules/FindOptiMathSat.cmake
@@ -1,21 +1,19 @@
 # Try to find OptiMathSat (http://optimathsat.disi.unitn.it/index.html)
 #
+# Requires OPTIMATHSAT_ROOT to be set.
+#
 # Once done this will define
 #  OptiMathSat_FOUND        - True if headers and libraries were found
 #  OptiMathSat_INCLUDE_DIRS - OptiMathSat include directories
 #  OptiMathSat_LIBRARIES    - OptiMathSat libraries to be linked
 
-if (OPTIMATHSAT_ROOT)
-    find_path(OptiMathSat_INCLUDE_DIR optimathsat.h PATHS "${OPTIMATHSAT_ROOT}/include")
-else ()
-    find_path(OptiMathSat_INCLUDE_DIR optimathsat.h)
+if (NOT DEFINED OPTIMATHSAT_ROOT OR OPTIMATHSAT_ROOT STREQUAL "")
+  message(FATAL_ERROR "OPTIMATHSAT_ROOT was not given!")
 endif ()
 
-if (OPTIMATHSAT_ROOT)
-    find_library(OptiMathSat_LIBRARY mathsat PATHS "${OPTIMATHSAT_ROOT}/lib")
-else ()
-    find_library(OptiMathSat_LIBRARY mathsat)
-endif ()
+find_path(OptiMathSat_INCLUDE_DIR optimathsat.h PATHS "${OPTIMATHSAT_ROOT}/include")
+
+find_library(OptiMathSat_LIBRARY mathsat PATHS "${OPTIMATHSAT_ROOT}/lib")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OptiMathSat DEFAULT_MSG OptiMathSat_LIBRARY OptiMathSat_INCLUDE_DIR)
